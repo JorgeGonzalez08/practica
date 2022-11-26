832d43ca811ab095a7d07fdf0dcdbb172fe98d3a
JorgeGonzalez08 <lx03099@innovaccion.mx>
2022-11-25 18:14:40 -0600

    cambios

---
+++ antesdebody.js
@@ -0,0 +1,7 @@
+window.onclick = function() {
+    let encabezados = document.createElement("h1");
+    const text = document.createTextNode("Quieres ser mi morrita");
+
+    encabezados.appendChild(text);
+    document.appendChild(encabezados);
+}
